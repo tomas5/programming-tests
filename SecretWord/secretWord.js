726c70af966086cbf89147ee49b731ffa816c8fb
Tomas Siugzdinis <hello@tomas5.com>
2015-11-22 19:53:36 +0000

    Add SecretWord test

---
+++ SecretWord/secretWord.js
@@ -0,0 +1,17 @@
+var your_drink;
+
+var reverse = function(s) {
+	return s.split("").reverse().join("");
+}
+
+var bartender = {
+	str1: "ers",
+	str2: reverse("rap"),
+	str3:"amet",
+	request:function(preference) {
+		return preference+".Secret word: "
+		+ this.str2 + this.str3 + this.str1;
+	}
+};
+
+bartender.request(your_drink);
