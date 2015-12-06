32d02bf1e1003c1a83b4a52950ecbb61b284436b
Tomas Siugzdinis <hello@tomas5.com>
2015-12-06 20:20:57 +0000

    Add JavaScript test

---
+++ JavaScript/JavaScript test.js
@@ -0,0 +1,71 @@
+﻿// Which of the following methods does the "document" object have?
+
+// Options:
+
+getElementsByTagName
+getElementById
+querySelectorAll
+getElementsBySelector
+getElementByClassName
+
+// Answer: https://developer.mozilla.org/en-US/docs/Web/API/Document
+
+// Document.getElementsByTagName() - Returns a list of elements with the given tag name.
+
+// document.getElementById(String id) - Returns an object reference to the identified element.
+
+// document.querySelectorAll(String selector) - Returns a list of all the Element nodes within the document that match the specified selectors.
+
+//-------------------------------------------------------------------
+
+// Which of the following statements are true?
+
+// 1. JavaScript can react to events.
+
+// 2. JavaScript can create cookies.
+
+// 3. JavaScript can read and write HTML elements.
+
+//-------------------------------------------------------------------
+
+// Which of the following is the correct way to append a value to an array in JavaScript?
+
+// 1.
+arr[arr.length - 1] = value
+
+// 2.
+arr.insert(value)
+
+// 3.
+arr.insert(arr.length, value)
+
+// 4.
+arr[arr.length + 1] = new Arrays(value)
+
+// 5.
+arr.add(value)
+
+// 6.
+arr[arr.length] = value
+
+// 7.
+None of the above
+
+
+//-------------------------------------------------------------------
+
+// Which of the following would be the correct way to read the "age" property of a "person" object?
+
+// 1.
+person['age']
+
+// 2.
+person.age
+
+// 3.
+person::age
+
+// 4.
+person[age]
+
+//-------------------------------------------------------------------
